de7bb8ec050cdacf0da6eae4802e3b57fdb06512
QliQ.dev <abbas.fatullaev@gmail.com>
2023-10-14 00:44:38 +0600

    temp disabling screen off

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,76 @@
+import type { PluginListenerHandle } from '@capacitor/core';
+declare module '@capacitor/cli' {
+    interface PluginsConfig {
+        /**
+         * These configuration values are available:
+         */
+        PrivacyScreen?: {
+            /**
+             * Configure whether the plugin should be enabled from startup.
+             *
+             * Only available for Android and iOS.
+             *
+             * @default true
+             * @example true
+             */
+            enable?: boolean;
+            /**
+             * Configure whether the plugin should display a custom image from assets instead of a default background gray for the privacy screen.
+             *
+             * Only available for iOS.
+             *
+             * @default ""
+             * @example "Splashscreen"
+             */
+            imageName?: string;
+        };
+    }
+}
+export interface PrivacyScreenPlugin {
+    /**
+     * Enables the privacy screen protection.
+     *
+     * Only available for Android and iOS.
+     *
+     * @since 1.1.0
+     */
+    enable(): Promise<void>;
+    /**
+     * Disables the privacy screen protection.
+     *
+     * Only available for Android and iOS.
+     *
+     * @since 1.1.0
+     */
+    disable(): Promise<void>;
+    /**
+     * Called when the screen recording is started.
+     *
+     * Only available on iOS for now.
+     *
+     * @since 3.0.2
+     */
+    addListener(eventName: 'screenRecordingStarted', listenerFunc: () => void): Promise<PluginListenerHandle> & PluginListenerHandle;
+    /**
+     * Called when the screen recording is stopped.
+     *
+     * Only available on iOS for now.
+     *
+     * @since 3.0.2
+     */
+    addListener(eventName: 'screenRecordingStopped', listenerFunc: () => void): Promise<PluginListenerHandle> & PluginListenerHandle;
+    /**
+     * Called when the screenshot is taken.
+     *
+     * Only available on iOS for now.
+     *
+     * @since 3.2.0
+     */
+    addListener(eventName: 'screenshotTaken', listenerFunc: () => void): Promise<PluginListenerHandle> & PluginListenerHandle;
+    /**
+     * Remove all listeners for this plugin.
+     *
+     * @since 3.0.2
+     */
+    removeAllListeners(): Promise<void>;
+}
